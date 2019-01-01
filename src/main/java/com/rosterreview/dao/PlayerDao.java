18c9ed421693c1914578886902374b7a0e138171
johnagordon83 <johnagordon83@gmail.com>
2019-01-01 14:51:43 -0500

    Created a data access class for player data.

---
+++ src/main/java/com/rosterreview/dao/PlayerDao.java
@@ -0,0 +1,35 @@
+package com.rosterreview.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rosterreview.entity.Player;
+
+/**
+ * A data access class for {@Link Player} data.
+ */
+
+@Transactional
+@Repository
+public class PlayerDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    /**
+     * Retrieve {@Link Player} profile information from the
+     * {@Link DataSource}.
+     *
+     * @param id  The id corresponding to the requested player.
+     * @return      A Player containing profile information or
+     *               <code>null</code> if a player matching the id argument
+     *            could not be found.
+     */
+    public Player getPlayer(String id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Player.class, id);
+    }
+}
