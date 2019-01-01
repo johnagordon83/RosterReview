8c05486f135e08c4ac61e29ab94ca7404b49a4fb
johnagordon83 <johnagordon83@gmail.com>
2019-01-01 15:12:11 -0500

    Created a controller class to handle requests for player data.

---
+++ src/main/java/com/rosterreview/controller/PlayerController.java
@@ -0,0 +1,37 @@
+package com.rosterreview.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rosterreview.entity.Player;
+import com.rosterreview.service.PlayerService;
+
+/**
+ * PlayerController is an annotated Spring Controller class that handles
+ * requests for player data.
+ */
+
+@RequestMapping("/player")
+@RestController
+public class PlayerController {
+
+    @Autowired
+    private PlayerService playerService;
+
+    /**
+     * Request profile information for a specified {@Link Player}.
+     *
+     * @param id  A string identifier corresponding to the requested Player.
+     * @return    A JSON representation of the requested Player.
+     */
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Player getPlayer(@PathVariable("id") String id) {
+        return playerService.getPlayer(id);
+    }
+}
